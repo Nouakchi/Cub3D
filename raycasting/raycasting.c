286095f80cc1a75979a7056e810dcd186eacf2c2
Othmane Nouakchi <onouakch@e3r2p6.1337.ma>
2023-09-22 03:19:04 +0100

    norm raycasting

--- raycasting/raycasting.c
+++ raycasting/raycasting.c
@@ -6,54 +6,67 @@
 /*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 12:37:41 by onouakch          #+#    #+#             */
-/*   Updated: 2023/09/22 01:15:19 by onouakch         ###   ########.fr       */
+/*   Updated: 2023/09/22 03:17:53 by onouakch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/cub.h"
 
-int	up_cast(t_data *data, int start, double beta_angle)
+double	check_horz_inter(t_data *data)
 {
-    // check horizontal intersection
-
-    data->ray.x_step = 64 / tan(data->ray.angle * (M_PI / 180.0));
+	int	i;
+	int	j;
+	
+	data->ray.x_step = 64 / tan(data->ray.angle * (M_PI / 180.0));
     data->ray.y_step = -64;
-
-    data->ray.y_h_inter = ((int)floor((int)data->player.y_pos >> (int)WALL_SHIFT) << (int)WALL_SHIFT) - 1;;
-    data->ray.x_h_inter = data->player.x_pos + (data->player.y_pos - data->ray.y_h_inter) / tan(data->ray.angle * (M_PI / 180.0));
-
-    int i = (int)data->ray.y_h_inter  >> (int)WALL_SHIFT;
-	int j = (int)data->ray.x_h_inter  >> (int)WALL_SHIFT;
-
-    while (i < data->map_data.map_height && i >= 0 && j < data->map_data.map_width && j >= 0 && data->map_data.map[i][j] != '1')
+    data->ray.y_h_inter = ((int)floor((int)data->player.y_pos >> (int)WALL_SHIFT)
+	 << (int)WALL_SHIFT) - 1;;
+    data->ray.x_h_inter = data->player.x_pos + (data->player.y_pos 
+	- data->ray.y_h_inter) / tan(data->ray.angle * (M_PI / 180.0));
+    i = (int)data->ray.y_h_inter  >> (int)WALL_SHIFT;
+	j = (int)data->ray.x_h_inter  >> (int)WALL_SHIFT;
+    while (i < data->map_data.map_height && i >= 0 && j < data->map_data.map_width 
+	&& j >= 0 && data->map_data.map[i][j] != '1')
 	{
 		data->ray.y_h_inter += data->ray.y_step;
 		data->ray.x_h_inter += data->ray.x_step;
 		i = (int)data->ray.y_h_inter >> (int)WALL_SHIFT;
 		j = (int)data->ray.x_h_inter >> (int)WALL_SHIFT;
 	}
+	return ((double)fabs(data->player.y_pos - data->ray.y_h_inter) 
+	/ sin(data->ray.angle * (M_PI / 180.0)));
+}
 
-	double horz_dist = (double)fabs(data->player.y_pos - data->ray.y_h_inter) / sin(data->ray.angle * (M_PI / 180.0));
-    // check vertical
+void	get_first_inter(t_data *data)
+{
+	double	tmp;
 
-    data->ray.x_step = 64;
+	data->ray.x_step = 64;
     data->ray.y_step = 64 * tan(data->ray.angle * (M_PI / 180.0));
-
 	if (data->ray.angle >= 90)
 		data->ray.x_step *= -1;
+    tmp = 0;
+	if (data->ray.angle < 90)
+		tmp = 64;
+    data->ray.x_v_inter = ((int)floor((int)data->player.x_pos 
+	>> (int)WALL_SHIFT) << (int)WALL_SHIFT) + tmp;
+	data->ray.y_v_inter = data->player.y_pos - 
+	((data->ray.x_v_inter - data->player.x_pos) 
+	* tan(data->ray.angle * (M_PI / 180.0)));
+}
 
-    double tmp = (data->ray.angle < 90) ? 64 : 0;
-    data->ray.x_v_inter = ((int)floor((int)data->player.x_pos >> (int)WALL_SHIFT) << (int)WALL_SHIFT) + tmp;
-	data->ray.y_v_inter = data->player.y_pos - ((data->ray.x_v_inter - data->player.x_pos) * tan(data->ray.angle * (M_PI / 180.0)));
-
-
+double	check_vert_inter(t_data *data)
+{
+	int	i;
+	int	j;
+	
+	get_first_inter(data);
     i = (int)data->ray.y_v_inter  >> (int)WALL_SHIFT;
 	j = (int)data->ray.x_v_inter  >> (int)WALL_SHIFT;
-
 	if (data->ray.angle >= 90)
 		j--;
-
-    while (i < data->map_data.map_height && i >= 0 && j < data->map_data.map_width && j >= 0 && data->map_data.map[i][j] != '1')
+    while (i < data->map_data.map_height && i >= 0 
+	&& j < data->map_data.map_width && j >= 0 && data->map_data.map[i][j] != '1')
 	{
 		if (data->ray.angle < 90 )
 			data->ray.y_v_inter -= data->ray.y_step;
@@ -66,54 +79,57 @@ int	up_cast(t_data *data, int start, double beta_angle)
 		if (data->ray.angle >= 90)
 			j--;
 	}
+	return ((double)fabs(data->player.y_pos - data->ray.y_v_inter)
+	/ sin(data->ray.angle * (M_PI / 180.0)));
+}
 
+void	check_0_degrees(t_data *data, double *ray)
+{
+	if (data->ray.angle - 0.05859375 == 0 || data->ray.angle + 0.05859375 == 180)
+	{
+		data->last_ray = *ray;
+		data->last_x_inter = data->ray.x_inter;
+		data->last_y_inter = data->ray.y_inter;
+	}
+	if (data->ray.angle == 0 || data->ray.angle == 180)
+	{
+		*ray = data->last_ray;
+		data->ray.x_inter = data->last_x_inter;
+		data->ray.y_inter = data->last_y_inter;
+	}
+}
 
-	double vert_dist = (double)fabs(data->player.y_pos - data->ray.y_v_inter) / sin(data->ray.angle * (M_PI / 180.0));
+void	get_correct_ray(t_data *data, double *ray)
+{
+	double horz_dist;
+	double vert_dist;
 	
-
-	// calculate ray distance
-	
-
-	double ray = horz_dist;
+	horz_dist = check_horz_inter(data);
+	vert_dist = check_vert_inter(data);
+	*ray = horz_dist;
 	data->ray.is_vert = 0;
 	data->ray.x_inter = data->ray.x_h_inter;
 	data->ray.y_inter = data->ray.y_h_inter;
-	if (ray > vert_dist)
+	if (*ray > vert_dist)
 	{
-		ray = vert_dist;
+		*ray = vert_dist;
 		data->ray.is_vert = 1;
 		data->ray.x_inter = data->ray.x_v_inter;
 		data->ray.y_inter = data->ray.y_v_inter;
 	}
-	// if (data->ray.angle == 180)
-	// 	printf("[%f], [%f]\n", data->ray.x_inter, data->ray.y_inter);
-	
-	if (data->ray.angle - 0.05859375 == 0 || data->ray.angle + 0.05859375 == 180)
-	{
-		data->last_ray = ray;
-		data->last_x_inter = data->ray.x_inter;
-		data->last_y_inter = data->ray.y_inter;
-	}
-		
-	if (data->ray.angle == 0 || data->ray.angle == 180)
-	{
-		ray = data->last_ray;
-		data->ray.x_inter = data->last_x_inter;
-		data->ray.y_inter = data->last_y_inter;
-	}
+}
 
+int	up_cast(t_data *data, int start, double beta_angle)
+{
+	double ray;
+	get_correct_ray(data, &ray);
+	// 0 degrees hardcor
+	check_0_degrees(data, &ray);
 	// correct the fishbowl
-	
-
 	ray = fabs((double)ray * cos(beta_angle * (M_PI / 180.0)));
-	
-
 	// scale the ray
-
 	double actual_height = ceil((64 * 886) / ray);
-	
-
-	draw_line(data, start, 256 - (actual_height / 2), fabs(256 - (actual_height / 2)) + actual_height, actual_height);
-
+	draw_line(data, start, 256 - (actual_height / 2),
+	fabs(256 - (actual_height / 2)) + actual_height, actual_height);
     return (0);
 }
